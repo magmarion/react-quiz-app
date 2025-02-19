a8dc4e402f8ef1d380b93d8f01c84e92df920df8
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-19 11:06:53 +0100

    implement the home layout

--- src/HomeLayout.tsx
+++ src/HomeLayout.tsx
@@ -1,12 +1,20 @@
 
-import './index.css'
+import { Link } from 'react-router';
 
-function Home() {
-
+const Home = () => {
   return (
-    <>
-    <h1 className='text-3xl text-white font-bold'>Welcome Home</h1>
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='text-center'>
+        <h1 className='text-4xl mb-4'>Welcome to the Quiz App!</h1>
+        <p className='mb-4'>Select a quiz category to get started:</p>
+        <div>
+          <Link to="/quiz/react" className='btn btn-primary'>React Quiz</Link>
+          <Link to="/quiz/math" className='btn btn-secondary'>Math Quiz</Link>
+          <Link to="/quiz/astronomy" className='btn btn-tertiary'>Astronomy Quiz
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
