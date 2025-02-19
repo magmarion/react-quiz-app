bed2700582119747221b944de63bfaa09e60db9b
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-19 11:26:40 +0100

    use useParams hook to manage dynamic categories

--- src/HomeLayout.tsx
+++ src/HomeLayout.tsx
@@ -1,5 +1,5 @@
 
-import { Link } from 'react-router';
+import { Link, Outlet } from 'react-router';
 
 const Home = () => {
   return (
@@ -7,13 +7,14 @@ const Home = () => {
       <div className='text-center'>
         <h1 className='text-4xl mb-4'>Welcome to the Quiz App!</h1>
         <p className='mb-4'>Select a quiz category to get started:</p>
-        <div>
-          <Link to="/quiz/react" className='btn btn-primary'>React Quiz</Link>
-          <Link to="/quiz/math" className='btn btn-secondary'>Math Quiz</Link>
-          <Link to="/quiz/astronomy" className='btn btn-tertiary'>Astronomy Quiz
+        <div className='flex justify-center gap-2'>
+          <Link to="/quiz/react" className='btn btn-primary'>React</Link>
+          <Link to="/quiz/math" className='btn btn-secondary'>Math</Link>
+          <Link to="/quiz/astronomy" className='btn btn-tertiary'>Astronomy
           </Link>
         </div>
       </div>
+      <Outlet />
     </div>
   )
 }
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 import { Question } from "../data";
 
 const Quiz = () => {
+    const { category } = useParams<{ category: string }>();
     const [questions, setQuestions] = useState<Question[]>([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple")
+        let apiUrl = "";
+        switch (category) {
+            case "react":
+                apiUrl = "https://opentdb.com/api.php?amount=15&category=18&type=multiple";
+                break;
+            case "math":
+                apiUrl = "https://opentdb.com/api.php?amount=15&category=19&type=multiple&difficulty=hard"
+                break;
+            case "astronomy":
+                apiUrl = "https://opentdb.com/api.php?amount=15&category=17&type=multiple"
+                break;
+            default:
+                apiUrl = "https://opentdb.com/api.php?amount=15&category=18&type=multiple";
+
+        }
+
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
                 setQuestions(data.results);
                 setLoading(false);
             });
-    }, []);
+    }, [category]);
 
     return (
         <div>
-            <h2 className="text-2xl font-bold text-red-400">React Quiz</h2>
+            <h2 className="text-2xl font-bold text-blue-400">
+                {category?.toUpperCase()}
+            </h2>
             {loading ? (<p>Loading...</p>) : (
                 <div>
                     {questions.map((question, index) => (
