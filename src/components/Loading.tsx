ea542012ff0cdee66f372fb6de9e6df30d1e5fa9
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-21 11:21:11 +0100

    move the next button to its own file

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+// export default Loading = () => {
+//     return (
+//         if (loading) {
+//         return (
+//             <div className="text-center p-4">
+//                 <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mx-auto"></div>
+//                 <p className="mt-2">Loading questions...</p>
+//             </div>
+//         );
+//     }
+    
+//     )
+// }
+
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import NextButton from "../components/NextButton";
 import { Question } from "../types";
 
 const Quiz = () => {
@@ -82,6 +83,7 @@ const Quiz = () => {
         }
     };
 
+
     if (loading) {
         return (
             <div className="text-center p-4">
@@ -91,9 +93,11 @@ const Quiz = () => {
         );
     }
 
+
     if (error) {
         return <p className="text-red-500 text-center p-4">{error}</p>;
     }
+
 
     if (!questions.length) {
         return <p className="text-red-500 text-center p-4">
@@ -103,12 +107,14 @@ const Quiz = () => {
 
     const currentQuestion = questions[currentQuestionIndex];
 
+
     return (
         <div className="max-w-2xl mx-auto p-4">
             <div className="flex justify-between mb-4">
                 <h2 className="text-2xl font-bold text-blue-600">
                     {category?.toUpperCase()} QUIZ
                 </h2>
+
                 <div className="text-gray-300">
                     Question {currentQuestionIndex + 1}/{questions.length}
                 </div>
@@ -117,6 +123,7 @@ const Quiz = () => {
             <h3 className="text-xl font-semibold mb-6">
                 {currentQuestion.question}
             </h3>
+
             <div className="grid grid-cols-1 gap-4">
                 {currentQuestion.options.map((option, index) => (
                     <button
@@ -124,10 +131,12 @@ const Quiz = () => {
                         onClick={() => handleOptionClick(index)}
                         disabled={answerSubmitted}
                         className={`p-3 rounded-lg text-left transition-all duration-500 ease-in-out
+
                             ${answerSubmitted
                                 ? "cursor-not-allowed"
                                 : "cursor-pointer hover:scale-105"
                             } 
+                            
                              ${answerSubmitted
                                 ? index === currentQuestion.correctOption
                                     ? "bg-green-500 text-white"
@@ -146,17 +155,13 @@ const Quiz = () => {
                 <div className="text-gray-300">
                     Score: {score} / {questions.length}
                 </div>
-                <button
+                <NextButton
                     onClick={handleNextQuestion}
+                    isLastQuestion={currentQuestionIndex === questions.length - 1}
                     disabled={!answerSubmitted}
-                    className={`px-6 py-2 rounded-lg text-white transition-colors ${!answerSubmitted ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"
-                        }`}
-                >
-                    {currentQuestionIndex === questions.length - 1
-                        ? "Finish Quiz"
-                        : "Next Question"
-                    }
-                </button>
+                    score={score}
+                    totalQuestions={questions.length}
+                />
             </div>
         </div>
     );
