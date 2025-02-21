a85734b82b24fd512020272039c482998db84097
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-21 11:53:32 +0100

    add outlet to the homelayout page to make the page accesible from any other page

--- src/HomeLayout.tsx
+++ src/HomeLayout.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 
 const HomeLayout = () => (
     <div>
         <header className="p-4 bg-gray-800 text-white text-center">
-            <h1 className="text-2xl">Quiz Island</h1>
+            <Link to="/" className="inline-block cursor-pointer transform transition-transform duration-300 hover:scale-115 hover:font-bold">
+                <h1 className="text-2xl">Quiz Island</h1>
+            </Link>
         </header>
         <main className="p-6">
             <Outlet />
