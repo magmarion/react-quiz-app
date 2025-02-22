4c3307d30164e830f0da9ba7f0ca2168b1cbf5d6
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-22 15:04:58 +0100

    add question rendering logic in separate component

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,66 @@
+import { FC } from "react";
+import { Question } from "../types";
+
+interface QuestionCardProps {
+    question: Question;
+    currentQuestionIndex: number;
+    totalQuestions: number;
+    selectedAnswer: number | null;
+    answerSubmitted: boolean;
+    handleOptionClick: (index: number) => void;
+    category?: string;
+}
+
+const QuestionCard: FC<QuestionCardProps> = ({
+    question,
+    currentQuestionIndex,
+    totalQuestions,
+    selectedAnswer,
+    answerSubmitted,
+    handleOptionClick,
+    category,
+}) => (
+    <div>
+        <div className="flex justify-between mb-4">
+            <h2
+                className={`text-2xl font-bold ${category === "react"
+                    ? "text-blue-600"
+                    : category === "math"
+                        ? "text-green-600"
+                        : category === "astronomy"
+                            ? "text-purple-600"
+                            : "text-gray-600"
+                    }`}
+            >
+                {category?.toUpperCase()}
+            </h2>
+            <div className="text-gray-300">
+                Question {currentQuestionIndex + 1} of {totalQuestions}
+            </div>
+        </div>
+        <h3 className="text-xl font-semibold mb-6">{question.question}</h3>
+        <div className="grid grid-cols-1 gap-4">
+            {question.options.map((option, index) => (
+                <button
+                    key={option}
+                    onClick={() => handleOptionClick(index)}
+                    disabled={answerSubmitted}
+                    className={`p-3 rounded-lg text-left transition-all duration-500 ease-in-out
+                     ${answerSubmitted ? "cursor-not-allowed" : "cursor-pointer hover:scale-105"}
+                     ${answerSubmitted
+                            ? index === question.correctOption
+                                ? "bg-green-500 text-white"
+                                : "bg-orange-500 text-white"
+                            : selectedAnswer == index
+                                ? "bg-blue-500 text-white"
+                                : "bg-gray-200 text-gray-600"
+                        }`}
+                >
+                    {option}
+                </button>
+            ))}
+        </div>
+    </div >
+)
+
+export default QuestionCard;
