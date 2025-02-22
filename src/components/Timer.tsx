2aa99345680a5763be8d9124c2a3bac3476bf722
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-22 15:24:54 +0100

    add interface and Timer function

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,25 @@
 import { FC, useEffect, useState } from "react";
 
-const Timer: FC = () => {
+interface TimeProps {
+    intialTime: number;
+    onTimeUp: () => void;
+}
+
+const Timer: FC<TimeProps> = ({ intialTime, onTimeUp }) => {
     const [timeLeft, setTimeLeft] = useState(30);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            onTimeUp();
+            return;
+        }
+
+        const timer = setInterval(() => {
+            setTimeLeft((prevTime) => prevTime - 1);
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, [timeLeft, onTimeUp]);
 
 }
 
