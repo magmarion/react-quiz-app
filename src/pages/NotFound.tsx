511687e58c5f8ea186db41f53f91663a71ee3cf2
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-24 16:04:33 +0100

    add additional message to the user on not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,18 @@
+import { TbFaceIdError } from "react-icons/tb";
+
 function NotFound() {
 
-  return (
-    <>
-      <h1 className="text-2xl font-bold text-red-400">Page Not Found</h1>
-    </>
-  )
+    return (
+        <div className="h-full flex flex-col justify-center items-center gap-2">
+            <div className="flex items-center gap-2 text-2xl font-bold text-slate-400">
+                <TbFaceIdError size={40} />
+            <span>Page not found</span>
+            </div>
+            <h1 className="text-2xl font-bold text-slate-400 mt-2">
+                This site can't be reached.
+            </h1>
+        </div>
+    )
 }
 
 export default NotFound;
