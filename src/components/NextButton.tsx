73f8b4b09dfea836ef17d17a649a8c8da8c657a7
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-24 16:41:45 +0100

    add additional style on next btn

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -23,9 +23,10 @@ const NextButton: FC<NextButtonProps> = ({
         <button
             onClick={onClick}
             disabled={disabled}
-            className={`px-6 py-2 rounded-lg text-white transition-colors ${disabled
-                ? "bg-gray-400 cursor-not-allowed"
-                : "bg-blue-500 hover:bg-blue-600"
+            className={`px-6 py-2 rounded-lg text-white cursor-pointer transition-colors
+                 ${disabled
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-blue-500 hover:bg-blue-600"
                 }`}
         >
             {isLastQuestion ? "Finish Quiz" : "Next Question"}
