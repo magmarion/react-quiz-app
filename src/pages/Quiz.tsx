68ae4a433cffe67ebe40950b7540a6d3c015cf4e
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-02-24 12:48:26 +0100

    add JSDocs

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { MdError } from 'react-icons/md';
 import Loading from "../components/Loading";
 import NextButton from "../components/NextButton";
 import QuestionCard from "../components/QuestionCard";
@@ -7,6 +8,25 @@ import Timer from "../components/Timer";
 import { Question } from "../types";
 import Result from "./Result";
 
+/**
+ * The Quiz component serves as the main quiz interface for the application.
+ * It retrieves quiz questions based on the selected category, manages the quiz state,
+ * and renders the appropriate components such as the QuestionCard, Timer, and NextButton.
+ * 
+ * The component manages the following states:
+ * - `questions`: An array of quiz questions fetched from an external source.
+ * - `loading`: A boolean indicating the loading state of the questions.
+ * - `currentQuestionIndex`: The index of the currently displayed question.
+ * - `selectedAnswer`: The index of the currently selected answer option.
+ * - `answerSubmitted`: A boolean indicating if an answer has been submitted.
+ * - `quizCompleted`: A boolean indicating if the quiz has been completed.
+ * - `score`: The user's current score based on correct answers.
+ * - `error`: A string containing any error messages related to fetching questions.
+ * - `timeUp`: A boolean indicating if the time for the quiz has expired.
+ * 
+ * The component handles user interactions such as selecting an answer, advancing to the next question,
+ * and handling time expiration. It also displays loading and error states, and shows the result upon completion.
+ */
 
 const Quiz = () => {
     const { category } = useParams<{ category: string }>();
@@ -21,8 +41,19 @@ const Quiz = () => {
     const [timeUp, setTimeUp] = useState(false);
 
     useEffect(() => {
+        /**
+         * fetchData is an asynchronous function that fetches quiz questions
+         * from an external API based on the selected category.
+         * The function first waits for 2 seconds to simulate a loading delay,
+         * then fetches the questions from the API endpoint. If the response
+         * is not ok, an error is thrown. If the response data is not in the
+         * expected format, an error is thrown. If an error is thrown, the
+         * error message is stored in the component state, and the loading
+         * state is set to false.
+         */
         const fetchData = async () => {
             try {
+                await new Promise((resolve) => setTimeout(resolve, 2000));
                 let apiUrl = "";
                 switch (category) {
                     case "react":
@@ -33,6 +64,12 @@ const Quiz = () => {
                         break;
                     case "astronomy":
                         apiUrl = "https://magmarion.github.io/quiz-json/astronomy.json"
+                        break;
+                    case "physics":
+                        apiUrl = "https://magmarion.github.io/quiz-json/physics.json"
+                        break;
+                    case "error":
+                        apiUrl = ""
                         break;
                     default:
                         apiUrl = "https://magmarion.github.io/quiz-json/react.json";
@@ -63,6 +100,12 @@ const Quiz = () => {
         fetchData();
     }, [category]);
 
+    /**
+     * Handles user selection of an answer option.
+     * @param index The index of the selected answer option.
+     * If the user has not already submitted an answer, the selected answer is stored
+     * in the component state and the answer submitted state is set to true.
+     */
     const handleOptionClick = (index: number) => {
         if (!answerSubmitted) {
             setSelectedAnswer(index);
@@ -70,6 +113,13 @@ const Quiz = () => {
         }
     };
 
+    /**
+     * Handles user request to proceed to the next question.
+     * If the user has already submitted an answer, checks if the answer was correct
+     * and updates the score accordingly. Then resets the selected answer and answer
+     * submitted states. If the user has reached the end of the quiz, sets the quiz
+     * completed state to true. Otherwise, increments the current question index.
+     */
     const handleNextQuestion = () => {
         if (selectedAnswer === questions[currentQuestionIndex]?.correctOption) {
             setScore(score + 1);
@@ -84,6 +134,10 @@ const Quiz = () => {
         }
     };
 
+    /**
+     * Handles the event when the quiz time expires.
+     * Sets the `timeUp` state to true and marks the quiz as completed.
+     */
     const handleTimeUp = () => {
         setTimeUp(true);
         setQuizCompleted(true);
@@ -91,7 +145,11 @@ const Quiz = () => {
 
 
     if (loading) return <Loading />;
-    if (error) return <p className="text-red-500 text-center p-4">{error}</p>;
+
+    if (error) return (
+        <p className="text-red-500 text-xl text-center p-4"> <MdError className="mx-auto mb-4 text-3xl" />{error}</p>
+    );
+
     if (!questions.length) return <p className="text-red-500 text-center p-4"> No questions available.</p>;
     if (quizCompleted) return <Result score={score} total={questions.length} />;
 
